b57b7d58abbf0d0226b51d1a44825903d0ccce03
Thiago Lopes <thiagoslopes8@outlook.com>
2025-07-10 16:00:44 -0300

    refactor(app): wrap App with ThemeProvider and GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import HomePage from '@/pages/index';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/global';
+import { lightTheme } from '@/styles/theme';
 
 function App() {
-  return <HomePage />;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <HomePage />
+    </ThemeProvider>
+  );
 }
 
 export default App;
