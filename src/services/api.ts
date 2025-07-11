eb1ac161b04401f717ad29119c30542dcbbcf289
Thiago Lopes <thiagoslopes8@outlook.com>
2025-07-11 13:36:39 -0300

    chore(api): configure api via env vars

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import type { Employee } from '@/types/employee';
 
 export class ApiService {
-  private readonly API_URL = 'http://localhost:3000/employees';
+  private readonly API_URL = import.meta.env.VITE_API_URL;
 
   async fetchEmployees(): Promise<Employee[]> {
     const response = await fetch(this.API_URL);
